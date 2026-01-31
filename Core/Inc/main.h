793a6dfac37f824592a3b90e4cfac7a7877d9f12
taeeunindaeyo <ktyy0402@naver.com>
2026-01-31 19:38:43 +0900

    nRF 통신 2 차 수정

---
+++ Core/Inc/main.h
@@ -0,0 +1,12 @@
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#include "stm32f1xx.h"
+#include "common.h"
+#include "rcc.h"
+#include "delay.h"
+#include "gpio.h"
+#include "spi.h"
+#include "nrf24l01p.h"
+
+#endif
--- Core/Inc/stm32f1xx.h
+++ Core/Inc/stm32f1xx.h
@@ -1,6 +1,8 @@
 #ifndef __STM32F1XX_H
 #define __STM32F1XX_H
 
-#include "stm32f1xx.h"
+#include <stdint.h>
+
+#include "stm32f103xb.h"
 
 #endif
